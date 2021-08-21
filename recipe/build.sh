581f32c4ce49f59d2ac4ac08921a41cb7771efb0
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-21 16:58:30 +0200

    recipe: account for cross compilation specificities

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,18 @@
 mkdir build
 cd build
 
+export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+echo $BUILD_NUMPY_INCLUDE_DIRS
+echo $TARGET_NUMPY_INCLUDE_DIRS
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+fi
+
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_WITH_URDF_SUPPORT=ON \
@@ -13,3 +25,10 @@ cmake ${CMAKE_ARGS} .. \
 
 make -j${CPU_COUNT}
 make install
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo $BUILD_PREFIX
+  echo $PREFIX
+  sed -i.back 's|'"$BUILD_PREFIX"'|'"$PREFIX"'|g' $PREFIX/lib/cmake/pinocchio/pinocchioTargets.cmake
+  rm $PREFIX/lib/cmake/pinocchio/pinocchioTargets.cmake.back
+fi
