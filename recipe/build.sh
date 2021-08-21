80201c03527bcbc4b65dcabbdf329a66f18f4d1f
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-21 16:55:32 +0200

    recipe: simplify build

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,8 @@
 mkdir build
 cd build
 
-cmake .. \
+cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
       -DBUILD_WITH_URDF_SUPPORT=ON \
       -DBUILD_WITH_COLLISION_SUPPORT=ON \
       -DBUILD_WITH_OPENMP_SUPPORT=ON \
