9d6d76de851afd4cc6beb979d2c64026a6e72f59
Joris Vaillant <joris.vaillant@inria.fr>
2024-05-28 12:01:26 +0200

    Don't activate cppadcodegen on linux aarch64 and linux ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,6 +17,14 @@ if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
   export GENERATE_PYTHON_STUBS=0
 fi
 
+# cppadcodegen package doesn't exists on linux_aarch64 and linux_ppc64le architecture
+export BUILD_WITH_CODEGEN_SUPPORT=1
+if [[ $HOST =~ linux ]]; then
+  if [[ $HOST =~ aarch64 || $HOST =~ ppc64le ]]; then
+    export BUILD_WITH_CODEGEN_SUPPORT=0
+  fi
+fi
+
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_WITH_COLLISION_SUPPORT=ON \
