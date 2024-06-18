fa6742b5546e8d6a5ee5fe2f23910267bf1af3f7
Joris Vaillant <joris.vaillant@inria.fr>
2024-06-18 14:03:27 +0200

    Remove cppadcodegen and codegen support

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,20 +17,12 @@ if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
   export GENERATE_PYTHON_STUBS=0
 fi
 
-# cppadcodegen package doesn't exists on linux_aarch64 and linux_ppc64le architecture
-export BUILD_WITH_CODEGEN_SUPPORT=1
-if [[ $HOST =~ linux ]]; then
-  if [[ $HOST =~ aarch64 || $HOST =~ powerpc64le ]]; then
-    export BUILD_WITH_CODEGEN_SUPPORT=0
-  fi
-fi
-
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_WITH_COLLISION_SUPPORT=ON \
       -DBUILD_WITH_CASADI_SUPPORT=ON \
       -DBUILD_WITH_AUTODIFF_SUPPORT=ON \
-      -DBUILD_WITH_CODEGEN_SUPPORT=$BUILD_WITH_CODEGEN_SUPPORT \
+      -DBUILD_WITH_CODEGEN_SUPPORT=OFF \
       -DBUILD_WITH_EXTRA_SUPPORT=ON \
       -DBUILD_WITH_OPENMP_SUPPORT=ON \
       -DBUILD_PYTHON_BINDINGS_WITH_BOOST_MPFR_SUPPORT=ON \
